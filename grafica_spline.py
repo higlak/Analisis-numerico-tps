95743b734a72605d00a09fbb360aa981bfc359b2
Fran Rovira <frovira@fi.uba.ar>
2022-06-26 21:09:49 -0300

    Agregado de clase Spline y grafica

---
+++ grafica_spline.py
@@ -0,0 +1,64 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from prueba import *
+
+def elegir_spline(vector_splines, x):
+    for spline in vector_splines:
+        if spline.esta_dentro_del_intervalo(x):
+            return spline
+    raise ValueError('Valor no esta dentro del dominio de las splines')
+
+def imagen_de_puntos_por_spline(vector_splines, vector_puntos):
+    imagen = []
+    for x in vector_puntos:
+        spline_elegida = elegir_spline(vector_splines, x)
+        imagen.append(spline_elegida.evaluar_en(x))
+    return imagen
+
+def graficar_por_spline(vector_puntos_dominio, vector_puntos_imagen):
+    plt.grid()
+    plt.plot(vector_puntos_dominio,vector_puntos_imagen)
+    plt.title('Mauasa Katai')
+    plt.xlabel('Posicion')
+    plt.ylabel('Culo')
+    plt.xlim(1,30)
+    plt.show()
+    
+
+def main():
+    
+    vector_nodos_1 = [1,2,5,6,7,8,10,13,17]
+    vector_imagen_1 = [3,3.7,3.9,4.2,5.7,6.6,7.1,6.7,4.5]
+    derivada_0_1 = 1
+    derivada_n_1 = -2/3
+
+    vector_nodos_2 = [17,20,23,24,25,27,27.7]
+    vector_imagen_2 = [4.5,7,6.1,5.6,5.8,5.2,4.1]
+    derivada_0_2 = 3
+    derivada_n_2 = -4
+
+    vector_nodos_3 = [27.7, 28, 29, 30]
+    vector_imagen_3 = [4.1, 4.3, 4.1, 3]
+    derivada_0_3 = 1/3
+    derivada_n_3 = -3/2
+    
+    vector_splines_1 = crear_splines(vector_nodos_1, vector_imagen_1, derivada_0_1, derivada_n_1)
+    vector_splines_2 = crear_splines(vector_nodos_2, vector_imagen_2, derivada_0_2, derivada_n_2)
+    vector_splines_3 = crear_splines(vector_nodos_3, vector_imagen_3, derivada_0_3, derivada_n_3)
+    
+
+    plt.scatter(vector_nodos_1, vector_imagen_1)
+    plt.scatter(vector_nodos_2, vector_imagen_2)
+    plt.scatter(vector_nodos_3, vector_imagen_3)
+
+    vector_splines_resultante = []
+    [vector_splines_resultante.append(elem_1) for elem_1 in vector_splines_1]
+    [vector_splines_resultante.append(elem_2) for elem_2 in vector_splines_2]
+    [vector_splines_resultante.append(elem_3) for elem_3 in vector_splines_3]
+
+    x = np.linspace(1,30,1000)
+    f_x = imagen_de_puntos_por_spline(vector_splines_resultante, x)
+
+    graficar_por_spline(x, f_x)
+
+main()
--- pseudo_spline.py
+++ pseudo_spline.py
@@ -1,6 +1,37 @@
 import numpy as np
 e = 2.7182
 
+class Spline:
+
+    def __init__(self, a, b, c, d, x_i, inicio, fin):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+        self.x_i = x_i
+        self.inicio = inicio
+        self.fin = fin
+
+    def __str__(self):
+        if self.x_i < 0:
+            return f'{self.a} + {self.b}(x+{abs(self.x_i)}) + {self.c}(x+{abs(self.x_i)})² + {self.d}(x+{abs(self.x_i)})³, {self.inicio} <= x <= {self.fin}'
+        return f'{self.a} + {self.b}(x-{self.x_i}) + {self.c}(x-{self.x_i})² + {self.d}(x-{self.x_i})³, {self.inicio} <= x <= {self.fin}'
+
+    def inicio(self):
+        return self.inicio
+
+    def fin(self):
+        return self.fin
+
+    def esta_dentro_del_intervalo(self, x):
+        return self.inicio <= x <= self.fin    
+
+    def evaluar_en(self, x):
+        if self.esta_dentro_del_intervalo(x):
+            return self.a + self.b*(x-self.x_i) + self.c*(x-self.x_i)**2 + self.d*(x-self.x_i)**3
+        raise ValueError(f'El punto {x} no esta dentro del intervalo [{self.inicio}, {self.fin}]')
+
+
 def spline_matricial(cant_nodos, nodo, func, derivada_0, derivada_n):
 	n = cant_nodos - 1
 	h = []
@@ -37,7 +68,7 @@ def armar_matriz_nula(n):
 		A.append(fila)
 	return A
 
-def spline(cant_nodos, nodo, func, derivada_0, derivada_n):
+def coeficientes_de_splines(cant_nodos, nodo, func, derivada_0, derivada_n):
 	vector_c, h = spline_matricial(cant_nodos, nodo, func, derivada_0, derivada_n)
 	vector_a = func[:]
 	vector_d = []
@@ -49,7 +80,13 @@ def spline(cant_nodos, nodo, func, derivada_0, derivada_n):
 	
 	return vector_a, vector_b, vector_c, vector_d
 	
-
+def crear_splines(vector_dominio, vector_imagen, derivada_0, derivada_n):
+    vector_splines = []
+    cant_nodos = len(vector_dominio)
+    a, b, c, d = coeficientes_de_splines(cant_nodos, vector_dominio, vector_imagen, derivada_0, derivada_n)
+    for i in range(cant_nodos - 1):
+        vector_splines.append(Spline(a[i], b[i], c[i], d[i], vector_dominio[i], vector_dominio[i], vector_dominio[i+1]))
+    return vector_splines
 #print(spline(4, [1,2,3,4], [2,4,6,8], 2, 2))
 #a, b = spline(4, [0,1,2,3], [1, e, e ** 2, e ** 3], 1, e ** 3)
 #print(a)
